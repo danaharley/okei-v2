73f9edb1043a0fa82440b0e3e96c38c97a8928be
danaharley <danaharley.github@gmail.com>
2024-05-03 02:34:46 +0700

    fix main nav on mobile responsive

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,12 +15,12 @@ export const MainVav = ({ routes, className }: MainVavProps) => {
   return (
     <nav className={cn(className)}>
       <ul className="md:mx-[70px]">
-        <li className="grid grid-cols-5">
+        <li className="m-1 grid grid-cols-5">
           {routes.map((route, index) => (
             <Link
               key={index}
               href={route.href}
-              className="m-0.5 place-self-center self-center rounded-lg px-8 py-5 hover:bg-okei-foreground"
+              className="m-0.5 flex w-full items-center justify-center rounded-lg py-5 hover:bg-okei-foreground"
             >
               <span className="sr-only">{route.label}</span>
               {route.icon}
